30e40bbf18c5c9fc7a1ba8c338fa25cedb062b29
Crowdbotics <team@crowdbotics.com>
2021-11-07 16:39:41 +0000

    Initial commit.

---
+++ backend/shazam_31864/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for shazam_31864 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'shazam_31864.settings')
+
+application = get_wsgi_application()
---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "shazam_31864",
+  url: "https://shazam_31864.botics.co",
+  api: "https://shazam_31864.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
